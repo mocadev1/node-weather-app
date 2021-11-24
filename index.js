15dd902d0c6c1050fbdf12f7f763a21aa923b8cc
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2021-11-23 21:57:26 -0600

    Added the searches class model.

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const { inquirerMenu, pause} = require('./helpers/inquirer');
+const {inquirerMenu, pause, readInput} = require('./helpers/inquirer');
+const Searches = require('./models/searches');
 
 const main = async () => {
+
+    const searches = new Searches();
     let opt;
 
     do {
 
         opt = await inquirerMenu();
-        console.log( opt );
 
-        await pause();
+        switch( opt ) {
+            case 1:
+                // Show message
+                const place = await readInput('City: ');
+                console.log(place);
 
-    } while (opt !== 0)
+
+                // Search places
+
+                // Select place
+
+                // Weather
+
+                // Show up results
+                console.log('\nCity information\n'.green);
+                console.log('City:', );
+                console.log('Lat:', );
+                console.log('Lng:', );
+                console.log('Temperature:', );
+                console.log('Min:', );
+                console.log('Max:', );
+                break;
+        }
+
+        if ( opt !== 0 ) await pause();
+
+    } while ( opt !== 0 )
 }
 
 main();
---
+++ models/searches.js
@@ -0,0 +1,17 @@
+class Searches {
+    history = ['Tegucigalpa', 'Madrid', 'San José'];
+
+    constructor() {
+        // TODO: Read database whether exists
+    }
+
+    async city ( place = '') {
+
+        // HTTP petition
+        console.log( place );
+
+        return [];
+    }
+}
+
+module.exports = Searches;
